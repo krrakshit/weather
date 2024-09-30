7bc7283148ba86e67f8c88f940cb3dedba779f8c
krrakshit <rkrkr365@gmail.com>
2024-10-01 00:23:41 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Weather from './components/Weather'
+// eslint-disable-next-line no-unused-vars
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
